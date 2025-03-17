75012a7ae6c59a4c628c6078b6fd8e5f69a91004
bfyqjzsgkqycf <19128317516@163.com>
2025-03-17 19:16:56 +0800

    feat: 添加棋子类的属性和构造函数
    添加了棋子类的属性x、y和color，并定义了棋子的直径。同时实现了棋子类的构造函数以及获取x、y和color属性的方法。

--- src/Chess.java
+++ src/Chess.java
@@ -1,2 +1,27 @@
+import java.awt.Color;
+
 public class Chess {
+    private int x; // 棋子的 x 坐标索引
+    private int y; // 棋子的 y 坐标索引
+    private Color color; // 棋子颜色
+    public static int DIAMETER = 30; // 直径
+
+    public Chess(int x, int y, Color color) { // 棋子构造函数
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
 }
